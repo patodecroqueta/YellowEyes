bebbf5376b57809af3f14f6600bf64b54d389935
patodecroqueta <patodecroqueta@gmail.com>
2024-05-24 20:50:38 +0200

    a

--- Assets/scripts/AbrePuerta.cs
+++ Assets/scripts/AbrePuerta.cs
@@ -1,14 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
 
 public class AbrePuerta : MonoBehaviour
 {
     public Animator animator;
+
+    public TextMeshProUGUI textMeshProUGUI;
+
+    public GameObject visualKey;
+
     void OnTriggerEnter(Collider collider){
-        if (collider.gameObject.tag=="Player"){
-            animator.SetTrigger("abrir");
+        print("OnTriggerEnter");
+        if (collider.gameObject.GetComponent<Inventario>().HasItem("llave")){
+            animator.SetTrigger("Abrir"); 
+        } else {
+            //textMeshProUGUI.text="Look for the:";
+            //visualKey.SetActive(true);      
+
         }
+
+    }
+    void OnTriggerExit(Collider collider)
+    {
+        //textMeshProUGUI.text = "";
+        //visualKey.SetActive(false);
+
     }
 }
