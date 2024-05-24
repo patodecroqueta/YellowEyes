7e1806dbdc2ebcbc5f55f633e2a5503913bd1a2d
patodecroqueta <patodecroqueta@gmail.com>
2024-05-24 17:55:39 +0200

    qa

--- Assets/scripts/Remy.cs
+++ Assets/scripts/Remy.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Remy : MonoBehaviour
@@ -27,6 +29,7 @@ public class Remy : MonoBehaviour
         v = Input.GetAxis("Vertical");
         Animar();
         Mover();
+        Desplazar();
     }
     void Animar()
     {
@@ -54,9 +57,9 @@ public class Remy : MonoBehaviour
     {
         String animacion = GetComponent<Animator>().GetCurrentAnimatorClipInfo(0)[0].clip.name;
         //Si está cayendo, salimos
-        if (animacion=="Fall Flat") return;
+        if (animacion == "Fall Flat") return;
         //Si está levantando, salimos
-        if (animacion=="Standing Up") return;
+        if (animacion == "Standing Up") return;
         //Puedes continuar
         if (v > 0)
         {
@@ -68,8 +71,21 @@ public class Remy : MonoBehaviour
             {
                 speed = walkSpeed;
             }
-            transform.Translate(0, 0, v * speed * Time.deltaTime);
+            //transform.Translate(0, 0, v * speed * Time.deltaTime);
         }
         transform.Rotate(0, h * angularSpeed * Time.deltaTime, 0);
+    }
+    void Desplazar()
+    {
+        CharacterController cc = GetComponent<CharacterController>();
+        if (!cc.isGrounded)
+        {
+            cc.Move(-transform.up * speed * Time.deltaTime);
+        }
+        else
+        {
+            //cc.Move(transform.forward * cc.Move(transform.forward.x, -1, transform.forward.z * speed * Time.deltaTime); speed * Time.deltaTime);
+            cc.Move(new Vector3(transform.forward.x, -1, transform.forward.z) * v * speed * Time.deltaTime);
+        }
     }
 }
